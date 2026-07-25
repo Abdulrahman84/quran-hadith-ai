8c3a24cae9a827b1796758a1fedd5cfea5a383f3
Abdulrahman Horan <abdulrahmanhoran7@gmail.com>
2026-07-25 20:22:25 +0300

    Refine bilingual logo lockup

--- src/components/sanad-logo.tsx
+++ src/components/sanad-logo.tsx
@@ -8,16 +8,24 @@ export function SanadLogo({ className = "" }: SanadLogoProps) {
       aria-hidden="true"
       className={`sanad-logo ${className}`}
       fill="none"
-      viewBox="0 0 900 251"
+      viewBox="0 0 644 251"
     >
-      <svg height="251" viewBox="330 0 570 251" width="570" x="0" y="0">
+      <svg height="90" viewBox="356 41 413 123" width="301" x="0" y="45">
         <image
           height="251"
           href="/brand/sanad-logo-gold-forward.png?v=gold-forward"
           width="900"
         />
       </svg>
-      <svg height="251" viewBox="0 0 330 251" width="330" x="570" y="0">
+      <circle cx="124.3" cy="53.4" fill="var(--color-red)" r="9" />
+      <svg height="43" viewBox="529 195 301 43" width="301" x="0" y="164">
+        <image
+          height="251"
+          href="/brand/sanad-logo-gold-forward.png?v=gold-forward"
+          width="900"
+        />
+      </svg>
+      <svg height="251" viewBox="0 0 303 251" width="303" x="341" y="0">
         <image
           height="251"
           href="/brand/sanad-logo-gold-forward.png?v=gold-forward"
